910669f6df9f9da7b48a21efa20b35bd3bed8cb1
[topic5487] <[topic5487@gmail.com]>
2022-01-25 20:46:37 +0800

    fix permission

--- comment board(PDO)/handle_delete_comment.php
+++ comment board(PDO)/handle_delete_comment.php
@@ -1,15 +1,17 @@
 <?php 
 session_start();
 require_once("connect.php");
+$user_id = $_SESSION['username'];
 $id = $_GET['id'];
 if(empty($_GET['id'])){
     header('Location: index.php?errcode=1');
     die("資料不齊全");
 }
 //soft delete
-$sql = "UPDATE comments SET deleted_date=NOW() WHERE `id` =:id";
+$sql = "UPDATE comments SET deleted_date=NOW() WHERE id =:id AND user_id = :user_id";
 $statement = $conn->prepare($sql);
 $statement->bindParam('id', $id, PDO::PARAM_INT);
+$statement->bindParam('user_id', $user_id, PDO::PARAM_STR);
 $statement->execute();
 
 
--- comment board(PDO)/handle_update_comment.php
+++ comment board(PDO)/handle_update_comment.php
@@ -1,14 +1,16 @@
 <?php 
 session_start();
 require_once("connect.php");
+$user_id = $_SESSION['username'];
 $content = $_POST['content'];
 $id = $_POST['id'];
 if(empty($_POST['content'])){
     header('Location: update_comment.php?errcode=1&id='.$_POST['id']);
     die("資料不齊全");
 }
-$sql = "UPDATE comments SET `content`=:content WHERE `id` =:id";
+$sql = "UPDATE comments SET `content`=:content WHERE id =:id AND user_id = :user_id";
 $statement = $conn->prepare($sql);
+$statement->bindParam('user_id', $user_id, PDO::PARAM_STR);
 $statement->bindParam('content', $content, PDO::PARAM_STR);
 $statement->bindParam('id', $id, PDO::PARAM_INT);
 $statement->execute();
